72f87bcab08a4c1bf0b73f3a31df0c908f1d191a
Shaahin <shaahin@gmail.com>
2016-04-28 21:08:43 -0500

    Offset param added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var Bing = require('node-bing-api')({ accKey: "Zixegkgk40uFLU03DOjsYxt2M8MGk2Vu7Ie8AONS99U=" }); // https://www.npmjs.com/package/node-bing-api
+var bodyParser = require('body-parser');
 var app = express();
 
+var phrase, search_abstraction_result;
 app.set('port', (process.env.PORT || 5000));
+
+// Config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(express.static(__dirname + '/public'));
 
@@ -10,13 +16,15 @@ app.set('port', (process.env.PORT || 5000));
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
 
-var search_abstraction_result = [];
-app.get('/:phrase', function(req, res) {
+app.get('/', function(q, r) {
+  r.send("Use format /YourSearchPhrase/[?offset=n]")
+})
 
-});
+app.get('/:_phrase', function(q, r) {
+  search_abstraction_result = [];
+  var offset = q.query.offset
 
-app.param('phrase', function(q, r, next, phrase){
-  Bing.images(phrase, {top: 5}, function(error, res, body){
+  Bing.images(phrase, {top: offset }, function(error, res, body){
     for (var i = 0; i < body.d.results.length; i++ ) {
       var temp_json_obj = new Object();
       temp_json_obj.snippet = body.d.results[i].Title
@@ -27,36 +35,15 @@ app.param('phrase', function(q, r, next, phrase){
     }
     r.send( search_abstraction_result );
   });
-  //next();
 });
+
+app.param('_phrase', function(q, r, next, _phrase){
+  phrase = _phrase;
+  next()
+});
+
 
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-var dateToJson = function (date) {
-  var dateobj;
-  if (isNaN(date) == false) {
-    dateobj = new Date(date * 1000) // converts it to milliseconds
-  } else {
-    if ( isNaN(Date.parse(date)) == false  ) {
-      dateobj = new Date(date)
-    }
-    else {
-      dateobj = null;
-    }
-  }
-
-  // compose JSON
-  if (dateobj == null)
-    return {
-      unix: null,
-      natural: null
-    }
-  else
-    return {
-      unix: dateobj.getTime() / 1000,
-      natural: dateobj.toDateString()
-    }
-}
